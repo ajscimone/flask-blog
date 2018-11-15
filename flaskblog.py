13b3f6d4d441c4ad10d286bc1e0a95cba60dabfc
ajscimone <ajscimone@live.com>
2018-11-15 15:24:35 -0700

    Forms, Registration and Login Routes
    Added login and registration form classes using flask WTF forms.
    Created login and registration routes and templates. Input flash messaging for errors. Updated Layout for flash messages. Currently no database interations.

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '7253ea2c384b91c2bb27c02ecf28bc7e420c4457757a3c67a72058137211a3c1a577ab91343f30371b8ae33a18bd0673b21a4494464e0f87ead925bbcad73cd0'
 
 posts = [
     {
@@ -27,6 +31,27 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success') 
+        #f string formatting here requires python 3.6. Could also use the .format() method for previous versions
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
