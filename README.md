c3e9d2aae63e7cbcef09aa5d0c1c13d72564863a
ajscimone <ajscimone@live.com>
2018-11-29 22:48:57 -0700

    Initial SQL Alchemy database setup
    Created flask sql alchemy classes for user and post schemes.

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Then you can run the program with:
 
 You can also run it with python
 "python flaskblog.py"
+
+
+In order to create the database, open python in the directory of the project and run the following commands:
+"from flaskblog import db"
+"db.create_all()"
--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,35 @@
+
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '7253ea2c384b91c2bb27c02ecf28bc7e420c4457757a3c67a72058137211a3c1a577ab91343f30371b8ae33a18bd0673b21a4494464e0f87ead925bbcad73cd0'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True) #Setting lazy to true here causes sql to simply pull all the posts
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
@@ -54,4 +80,4 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
